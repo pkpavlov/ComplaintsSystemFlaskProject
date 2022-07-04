9c52f1284568e360b22cced5515add6a194f5646
Pavel Pavlov <pkpavlov87@gmail.com>
2022-07-04 21:30:39 +0300

    Create models for complaint system

---
+++ models/enums.py
@@ -0,0 +1,14 @@
+from enum import Enum
+
+
+class UserRole(Enum):
+    complainer = "Complainer"
+    approver = "Approver"
+    admin = "Admin"
+
+
+class ComplaintState(Enum):
+    pending = "Pending"
+    approved = "Approved"
+    rejected = "Rejected"
+
